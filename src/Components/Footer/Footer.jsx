4b3e22abbd4c4e0ff3d5da3b63ddbcd5afa19cce
Mary <gretchen7887@gmail.com>
2024-11-13 21:51:15 +0400

    Added Footer & Main

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
 import './footer.scss'
+import video2 from '../../assets/video2.mp4'
+import { FiSend } from 'react-icons/fi'
+import { MdOutlineTravelExplore } from 'react-icons/md'
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import { FaYoutube } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { SiTripadvisor } from 'react-icons/si'
+import { FaChevronRight } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter email address" />
+            <button className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a className="logo flex" href="#">
+                <MdOutlineTravelExplore className="icon" /> TRAVEL
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloremque officiis perspiciatis voluptatum quos ipsa aliquid,
+              praesentium accusantium et ipsum ea non nemo nam magnam enim,
+              quod, minima earum dolores reiciendis!
+            </div>
+
+            <div className="footerSocials flex">
+              <FaSquareXTwitter className="icon" />
+              <FaYoutube className="icon" />
+              <FaInstagram className="icon" />
+              <SiTripadvisor className="icon" />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FaChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FaChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FaChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FaChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FaChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './main.scss'
 import data from '../../constants/data'
+import { IoLocationOutline } from 'react-icons/io5'
+import { BiCheckboxChecked } from 'react-icons/bi'
 
 const Main = () => {
   return (
@@ -11,14 +13,39 @@ const Main = () => {
 
       <div className="secContent grid">
         {data.map(
-          ({id, imgSrc, destTitle, location, grade, fees, description}) => {
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
             return (
               <div key={id} className="singleDestination">
-                { 
                 <div className="imageDiv">
-                  <img src={imgSrc} alt="destinationImg" />
+                  <img src={imgSrc} alt={destTitle} loading="lazy" />
                 </div>
-                }
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <IoLocationOutline className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {grade}  <small>+1</small>
+                      </span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+
+                  <button className="btn flex">
+                    DETAILS <BiCheckboxChecked className="icon" />
+                  </button>
+                </div>
               </div>
             )
           }
